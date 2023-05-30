fbb1035303eb9c07b2b82298108d357c2fa9f45b
hilmiITT440 <hilmihakim94@gmail.com>
2023-05-30 11:35:53 +0800

    initial git push

---
+++ Scripts/ArchiveNote/Script1685199952234.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.socialnmobile.dictapps.notepad.color.note')
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.LinearLayout'), 0)
+
+Mobile.setText(findTestObject('Object Repository/ArchiveNote/android.widget.EditText'), 'archive title', 0)
+
+Mobile.setText(findTestObject('Object Repository/ArchiveNote/android.widget.EditText (1)'), 'archive message', 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.ImageButton (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.LinearLayout (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.Button - REMIND ME IN 15 MINUTES'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.Button - DONE'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.ImageButton (2)'), 0)
+
+Mobile.tap(findTestObject('ArchiveNote/android.widget.ImageButton_3dots'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.RelativeLayout'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.Button - OK'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.ImageView'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ArchiveNote/android.widget.ImageView (1)'), 0)
+
